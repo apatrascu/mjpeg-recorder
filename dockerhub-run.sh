3afe28bd51bd251ddbde88e22d8f3df75b69e894
Alecsandru Patrascu <alecsandru.patrascu@gmail.com>
2019-03-05 22:28:23 +0200

    Update dockerhub-run.sh

--- dockerhub-run.sh
+++ dockerhub-run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 docker pull palecsandru/mjpeg-recorder
-docker run -dit --restart=always --name mjpegrecorder -v `pwd`/video:/root/video palecsandru/mjpeg-recorder
+docker run -dit --restart=always --name mjpegrecorder -v /home/user/Dropbox/camera-persistent-storage:/root/video palecsandru/mjpeg-recorder
+# docker run -dit --restart=always --name mjpegrecorder -v `pwd`/video:/root/video palecsandru/mjpeg-recorder
